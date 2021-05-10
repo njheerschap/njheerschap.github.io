0c66e2e1e441fb563231fb1ba91e13c1f0090fce
njheerschap <78928590+njheerschap@users.noreply.github.com>
2021-05-09 21:19:26 -0400

    First Commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  
+  // Navigation arrows
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
